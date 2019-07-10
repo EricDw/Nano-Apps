408a9a17756500cce5e6e66421d1fc8fad99ff72
Eric De Wildt <dewildte@gmail.com>
2019-07-10 00:41:27 -0400

    Add bottom app bar

--- app/src/main/java/com/publicmethod/nanoapps/MainActivity.kt
+++ app/src/main/java/com/publicmethod/nanoapps/MainActivity.kt
@@ -1,40 +1,64 @@
 package com.publicmethod.nanoapps
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getDrawable
+import com.google.android.material.bottomappbar.BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+import com.publicmethod.viewfactories.factories.createBottomAppBar
 import com.publicmethod.viewfactories.factories.createConstraintLayout
+import com.publicmethod.viewfactories.factories.createCoordinatorLayout
 import com.publicmethod.viewfactories.factories.createFloatingActionButton
-import com.publicmethod.viewfactories.theme.constrainFAB
-import com.publicmethod.viewfactories.theme.themeWithConstraintMargins
+import com.publicmethod.viewfactories.theme.themeWithBottomAppBarMargins
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val constraintLayout = createConstraintLayout {
-            id = R.id.constraint_layout_main
+        val rootLayout = createCoordinatorLayout {
+            id = R.id.coordinator_layout_main
         }.apply {
+
+            createConstraintLayout {
+                id = R.id.constraint_layout_main
+            }.also {
+                addView(it)
+            }
+
+            createBottomAppBar {
+                id = R.id.app_bar_main
+                fabAlignmentMode = FAB_ALIGNMENT_MODE_CENTER
+                backgroundTint = ColorStateList.valueOf(getColor(R.color.colorPrimaryDark))
+                navigationIcon = getDrawable(this@MainActivity, R.drawable.ic_menu_white_24dp)
+            }.also {
+                addView(it)
+                setSupportActionBar(it)
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            }
+
             createFloatingActionButton {
                 id = R.id.fab_main
                 setImageDrawable(
                     getDrawable(
                         this@MainActivity,
-                        R.drawable.ic_edit_white_24dp
+                        R.drawable.ic_edit_white_30dp
                     )
                 )
                 setOnClickListener {
                     showBottomInput()
                 }
-                themeWithConstraintMargins()
+                themeWithBottomAppBarMargins(
+                    R.id.app_bar_main,
+                    Gravity.CENTER
+                )
             }.also {
                 addView(it)
-                constrainFAB(it)
             }
         }
 
-        setContentView(constraintLayout)
+        setContentView(rootLayout)
     }
 
     private fun showBottomInput() =
