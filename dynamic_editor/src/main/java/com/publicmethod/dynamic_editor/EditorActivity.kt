5bb61a27c701194d9d1fb2f2b0cccf4f706596ad
EricD <eric.dewildt@compassdigital.io>
2019-07-13 15:07:42 -0400

    Add dynamic instant feature and support

---
+++ dynamic_editor/src/main/java/com/publicmethod/dynamic_editor/EditorActivity.kt
@@ -0,0 +1,78 @@
+package com.publicmethod.dynamic_editor
+
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomappbar.BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+import com.publicmethod.nanoapps.R
+import com.publicmethod.viewfactories.factories.createBottomAppBar
+import com.publicmethod.viewfactories.factories.createConstraintLayout
+import com.publicmethod.viewfactories.factories.createCoordinatorLayout
+import com.publicmethod.viewfactories.factories.createFloatingActionButton
+import com.publicmethod.viewfactories.theme.themeWithBottomAppBarMargins
+
+class EditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val rootLayout = createCoordinatorLayout {
+            id = R.id.coordinator_layout_main
+        }.apply {
+
+            createConstraintLayout {
+                id = R.id.constraint_layout_main
+            }.also {
+                addView(it)
+            }
+
+            createBottomAppBar {
+                id = R.id.app_bar_main
+                fabAlignmentMode = FAB_ALIGNMENT_MODE_CENTER
+                backgroundTint = ColorStateList.valueOf(getColor(R.color.colorPrimaryDark))
+                navigationIcon =
+                    ContextCompat.getDrawable(this@EditorActivity, R.drawable.ic_menu_white_24dp)
+            }.also {
+                addView(it)
+                setSupportActionBar(it)
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            }
+
+            createFloatingActionButton {
+                id = R.id.fab_main
+                setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this@EditorActivity,
+                        R.drawable.ic_edit_white_30dp
+                    )
+                )
+                setOnClickListener {
+                    showBottomInput()
+                }
+                themeWithBottomAppBarMargins(
+                    R.id.app_bar_main,
+                    Gravity.CENTER
+                )
+            }.also {
+                addView(it)
+            }
+        }
+
+        setContentView(rootLayout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean =
+        menuInflater.inflate(R.menu.menu_main, menu).run {
+            true
+        }
+
+    private fun showBottomInput() =
+        Toast.makeText(
+            this,
+            "Showing bottom modal",
+            Toast.LENGTH_LONG
+        ).show()
+}
