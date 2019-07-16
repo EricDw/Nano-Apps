b2af65a76b5eb822a3a4d7a30fbee95ecf65c923
EricD <eric.dewildt@compassdigital.io>
2019-07-16 00:07:03 -0400

    Tapping todos, take in commands instead of buttons.

--- app/src/main/java/com/publicmethod/nanoapps/MainActivity.kt
+++ app/src/main/java/com/publicmethod/nanoapps/MainActivity.kt
@@ -2,21 +2,25 @@ package com.publicmethod.nanoapps
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet.*
 import com.publicmethod.viewfactories.dp
 import com.publicmethod.viewfactories.factories.*
 import com.publicmethod.viewfactories.theme.applyConnections
-import com.publicmethod.viewfactories.theme.applyFABConstraints
 import com.publicmethod.viewfactories.theme.constrainToTopLeftAndRighOfParent
 import io.noties.markwon.Markwon
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val markwon = Markwon.create(this@MainActivity)
 
         createConstraintLayout constraint@{
 
@@ -38,27 +42,65 @@ class MainActivity : AppCompatActivity() {
 
                 with(this, ::constrainToTopLeftAndRighOfParent)
 
-            }.also { Markwon.create(this@MainActivity).setMarkdown(it, "# Nano Apps") }
+            }.also { markwon.setMarkdown(it, "# Nano Apps") }
 
-            createButton {
+            createTextView {
 
-                text = "LAUNCH EDITOR"
+                id = R.id.text_view_options
 
-                layoutParams = ViewGroup.MarginLayoutParams(layoutParams).apply {
-                    setMargins(0, dp(40), 0, 0)
-                }
-
-                setBackgroundColor(Color.BLACK)
                 setTextColor(Color.GREEN)
 
-                setOnClickListener {
-                    ActivityLauncher.launchEditor(this@MainActivity)
-                }
+                layoutParams = marginMatchParentWidthParams
 
                 with(this, ::addView)
 
                 applyConnections {
+                    connect(id, LEFT, PARENT_ID, LEFT)
+                    connect(id, RIGHT, PARENT_ID, RIGHT)
                     connect(id, TOP, R.id.text_view_title, BOTTOM)
+                    connect(id, BOTTOM, R.id.edit_text_main, TOP)
+                }
+
+            }.also {
+                markwon.setMarkdown(
+                    it,
+                    """
+                        ### Accepts the following commands:
+                        
+                        1. Editor  
+                    """.trimIndent()
+                )
+            }
+
+            createEditTextView {
+
+                id = R.id.edit_text_main
+
+                hint = "INPUT COMMAND"
+
+                isSingleLine = true
+
+                layoutParams = marginMatchParentWidthParams
+
+                setOnKeyListener { _, keyCode, keyEvent ->
+                    if (keyEvent.action == KeyEvent.ACTION_DOWN
+                        && keyCode == KeyEvent.KEYCODE_ENTER
+                    ) when (text.toString().trim().toLowerCase(Locale.getDefault())) {
+                            "1" -> ActivityLauncher.launchEditor(this@MainActivity)
+                            "1." -> ActivityLauncher.launchEditor(this@MainActivity)
+                            "editor" -> ActivityLauncher.launchEditor(this@MainActivity)
+                        }
+
+                    true
+                }
+
+                setBackgroundColor(Color.BLACK)
+                setHintTextColor(Color.GREEN)
+                setTextColor(Color.GREEN)
+
+                with(this, ::addView)
+
+                applyConnections {
                     connect(id, BOTTOM, PARENT_ID, BOTTOM)
                     connect(id, RIGHT, PARENT_ID, RIGHT)
                     connect(id, LEFT, PARENT_ID, LEFT)
@@ -66,18 +108,6 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-//            createFloatingActionButton {
-//
-//                setOnClickListener {
-//                    ActivityLauncher.launchEditor(
-//                        this@MainActivity
-//                    )
-//                }
-//
-//                with(this, ::addView)
-//
-//                with(this, ::applyFABConstraints)
-//            }
         }.also(::setContentView)
 
     }
--- dynamic_markdown_viewer/src/main/java/com/publicmethod/dynamic_markdown_viewer/ViewerActivity.kt
+++ dynamic_markdown_viewer/src/main/java/com/publicmethod/dynamic_markdown_viewer/ViewerActivity.kt
@@ -2,58 +2,90 @@ package com.publicmethod.dynamic_markdown_viewer
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.TextPaint
+import android.text.style.ClickableSpan
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.publicmethod.nanoapps.EXTRA_KEY_CONTENT
+import com.publicmethod.viewfactories.dp
 import com.publicmethod.viewfactories.factories.createTextView
 import com.publicmethod.viewfactories.factories.viewGroupMatchParentParams
+import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
+import io.noties.markwon.MarkwonSpansFactory
+import io.noties.markwon.SpanFactory
+import io.noties.markwon.ext.tasklist.TaskListItem
 import io.noties.markwon.ext.tasklist.TaskListPlugin
+import io.noties.markwon.ext.tasklist.TaskListSpan
 
 class ViewerActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(createContentView())
+        Markwon.builder(this@ViewerActivity)
+            .usePlugin(TaskListPlugin.create(this@ViewerActivity))
+            .usePlugin(object : AbstractMarkwonPlugin() {
+                override fun configureSpansFactory(builder: MarkwonSpansFactory.Builder) {
+
+                    // obtain original SpanFactory set by TaskListPlugin
+                    val origin = builder.getFactory(TaskListItem::class.java)
+                        ?: // or throw, as it's a bit weird state and we expect
+                        // this factory to be present
+                        return
+
+                    builder.setFactory(
+                        TaskListItem::class.java,
+                        SpanFactory { configuration, props ->
+                            // it's a bit non-secure behavior and we should validate
+                            // the type of returned span first, but for the sake of brevity
+                            // we skip this step
+                            val span = origin.getSpans(configuration, props) as TaskListSpan?
+                                ?: // or throw
+                                return@SpanFactory null
+
+                            // return an array of spans
+                            arrayOf(span, object : ClickableSpan() {
+                                override fun onClick(widget: View) {
+                                    // toggle VISUAL state
+                                    span.isDone = !span.isDone
+
+                                    // do not forget to invalidate widget
+                                    widget.invalidate()
+
+                                    // execute your persistence logic
+                                }
+
+                                override fun updateDrawState(ds: TextPaint) {
+                                    // no-op, so appearance is not changed (otherwise
+                                    // task list item will look like a link)
+                                }
+                            })
+                        })
+                }
+            }).build().run {
+                setContentView(createContentView(this))
+            }
+
     }
 
-    private fun createContentView(): View =
+    private fun createContentView(markwon: Markwon): View =
         createTextView(settings).apply {
             intent?.getStringExtra(EXTRA_KEY_CONTENT)?.run {
-
-                Markwon.builder(this@ViewerActivity).apply {
-                    usePlugin(TaskListPlugin.create(this@ViewerActivity))
-                }.build().setMarkdown(
+                markwon.setMarkdown(
                     this@apply, this
                 )
             }
         }
 
-//
-//    private fun createContentView(editTextFactory: Context.() -> TextView = basicEditTextFactory): CoordinatorLayout =
-//        createCoordinatorLayout {
-//            id = R.id.coordinator_layout_main
-//        }.apply {
-//
-//            createConstraintLayout {
-//                id = R.id.constraint_layout_main
-//            }.apply constraint@{
-//                editTextFactory().also(this@constraint::addView)
-//            }.also(::addView)
-//        }
-//
-//    private val basicEditTextFactory: Context.() -> EditText = {
-//        createEditTextView(settings)
-//    }
-
     private val settings: TextView.() -> Unit = {
         setTextColor(Color.GREEN)
         setBackgroundColor(Color.BLACK)
         gravity = Gravity.TOP or Gravity.START
-        layoutParams = viewGroupMatchParentParams
+        layoutParams = viewGroupMatchParentParams.apply {
+            setPadding(dp(16), dp(16), dp(16), dp(16))
+        }
     }
 
 }
